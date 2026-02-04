2b8fbbe07bb5a36044a698037673a03082084100
EalZz <114982277+EalZz@users.noreply.github.com>
2026-02-04 11:20:34 +0900

    KSJ/ fix precautions

--- src/multimodality/fashion_engine/config.py
+++ src/multimodality/fashion_engine/config.py
@@ -4,9 +4,11 @@ import torch
 @dataclass
 class FashionConfig:
     """시스템 환경 설정 및 경로 관리 클래스"""
+    ## DB 위치 확인해서 수정 필요
     base_dir: str = "/content/drive/MyDrive/LikeLion/최종 프로젝트 개인 임시/테스트용"
     db_path: str = "/content/fashion_items.db"
     chromadb_dir: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb"
     model_name: str = "patrickjohncyh/fashion-clip"
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     use_fp16: bool = True
+

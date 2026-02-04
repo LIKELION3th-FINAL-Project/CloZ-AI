c2ee31631053c36039876f69451fa166c794e88b
EalZz <114982277+EalZz@users.noreply.github.com>
2026-02-04 14:04:43 +0900

    KSJ / 변수 이름 변경

--- src/multimodality/fashion_engine/config.py
+++ src/multimodality/fashion_engine/config.py
@@ -7,12 +7,9 @@ class FashionConfig:
     ## DB 위치 확인해서 수정 필요
     base_dir: str = "/content/drive/MyDrive/LikeLion/최종 프로젝트 개인 임시/테스트용"
     db_path: str = "/content/fashion_items.db"
-    chromadb_dir: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb"
-    wardrobe_db_path: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb_mycloset"
+    chromadb_ref_dir: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb"
+    chromadb_war_dir: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb_mycloset"
     model_name: str = "patrickjohncyh/fashion-clip"
     model_img_path: str = "/content/model.webp"
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     use_fp16: bool = True
-
-
-

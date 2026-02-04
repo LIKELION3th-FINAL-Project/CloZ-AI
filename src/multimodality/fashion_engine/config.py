6684c7ce2565356412a0a900e907a460c4e004d2
EalZz <114982277+EalZz@users.noreply.github.com>
2026-02-04 12:19:02 +0900

    KSJ/fix recommander(chroma db)

--- src/multimodality/fashion_engine/config.py
+++ src/multimodality/fashion_engine/config.py
@@ -8,7 +8,11 @@ class FashionConfig:
     base_dir: str = "/content/drive/MyDrive/LikeLion/최종 프로젝트 개인 임시/테스트용"
     db_path: str = "/content/fashion_items.db"
     chromadb_dir: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb"
+    wardrobe_db_path: str = "/content/drive/MyDrive/멋쟁이사자처럼/project_final/chromadb_mycloset"
     model_name: str = "patrickjohncyh/fashion-clip"
+    model_img_path: str = "/content/model.webp"
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     use_fp16: bool = True
+
+
 
